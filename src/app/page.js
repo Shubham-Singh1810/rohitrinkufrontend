090bf44a61a390b0dc934d32de7409440380a38b
Shubham-Singh1810 <hittheshubham1810@gmail.com>
2025-01-07 19:35:00 +0530

    after adding the production url

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,43 @@
+"use client";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
 export default function Home() {
+  const [formData, setFormDta] = useState({
+    location: "",
+    fullName: "",
+    contact: "",
+    yourCity: "",
+    noOfKundali: "",
+    extraTime: "",
+    bookingDate: "",
+  });
+  const handleSubmitBooking = async () => {
+    if(formData.contact.length !=10){
+      toast.error("Please enter a valid contact number")
+      return
+    }
+    try {
+      let response = await axios.post("https://rohitrinkuserver.vercel.app/api/booking/store", formData);
+      if (response?.data.success) {
+        toast.success(response?.data.message);
+        setFormDta({
+          location: "",
+          fullName: "",
+          contact: "",
+          yourCity: "",
+          noOfKundali: "",
+          extraTime: "",
+          bookingDate: "",
+        });
+      } else {
+        toast.error("Something went wrong");
+      }
+    } catch (error) {
+      toast.error("Internal Server Error");
+    }
+  };
+
   return (
     <div>
       {/* hero section start */}
@@ -40,40 +79,71 @@ export default function Home() {
             <h1 className="mb-2">Booking Your Appointment</h1>
             <div className="col-md-7 ">
               <label>Select Location</label>
-              <slect className="form-control">
-                <option>Chandigarh</option>
-              </slect>
-
+              <select className="form-control" value={formData?.location} onChange={(e) => setFormDta({ ...formData, location: e.target.value })}>
+                <option value="">Select</option>
+                <option value="Chandigarh">Chandigarh</option>
+                <option value="Delhi">Delhi</option>
+              </select>
               <label>Full Name</label>
-              <input className="form-control" />
+              <input className="form-control" value={formData?.fullName} onChange={(e) => setFormDta({ ...formData, fullName: e.target.value })} />
               <label>Contact Number</label>
-              <input className="form-control" />
+              <input className="form-control" type="number" value={formData?.contact} onChange={(e) => setFormDta({ ...formData, contact: e.target.value })} />
               <label>Your City</label>
-              <input className="form-control" />
+              <input className="form-control" value={formData?.yourCity} onChange={(e) => setFormDta({ ...formData, yourCity: e.target.value })} />
               <label>Number of Kundali</label>
-              <slect className="form-control">
-                <option>1 (10 min)</option>
-              </slect>
+              <select
+                className="form-control"
+                onChange={(e) => setFormDta({ ...formData, noOfKundali: e.target.value })}
+                value={formData?.noOfKundali}
+              >
+                <option value="">Select</option>
+                <option value="1">1 (10 min)</option>
+                <option value="2">2 (20 min)</option>
+                <option value="3">3 (30 min)</option>
+                <option value="4">3 (40 min)</option>
+              </select>
               <label>Extra Time</label>
-              <input className="form-control" />
+              <input
+                className="form-control"
+                onChange={(e) => setFormDta({ ...formData, extraTime: e.target.value })}
+                value={formData?.extraTime}
+              />
               <label>Select Date</label>
-              <div className="mb-4 d-flex justify-content-between d-md-block">
+              {/* <div className="mb-4 d-flex justify-content-between d-md-block">
                 <button className="purpleBtn">20 Nov</button>
                 <button className="whiteBtn mx-md-3">21 Nov</button>
                 <button className="whiteBtn">22 Nov</button>
-              </div>
-
-              <div>
-                <button className=" submitBtn" style={{ background: "#139F01" }}>
-                  Submit
-                </button>
-              </div>
+              </div> */}
+              <input
+                className="form-control mb-4"
+                type="date"
+                onChange={(e) => setFormDta({ ...formData, bookingDate: e.target.value })}
+                value={formData?.bookingDate}
+              />
+              {formData?.location &&
+              formData?.fullName &&
+              formData?.contact &&
+              formData?.bookingDate &&
+              formData?.noOfKundali &&
+              formData?.extraTime ? (
+                <div className="w-100">
+                  <button className=" submitBtn" style={{ background: "#139F01" }} onClick={handleSubmitBooking}>
+                    Submit
+                  </button>
+                </div>
+              ) : (
+                <div className="w-100">
+                  <button className=" submitBtn" style={{ background: "#139F01", opacity: "0.5" }}>
+                    Submit
+                  </button>
+                </div>
+              )}
             </div>
-            
           </div>
         </div>
       </div>
       {/* booking section end */}
+      <ToastContainer />
     </div>
   );
 }
