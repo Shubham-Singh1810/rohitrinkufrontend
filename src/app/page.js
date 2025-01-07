c19b57fd9e40b28d055a9be7134516f7b4f2583d
Shubham-Singh1810 <hittheshubham1810@gmail.com>
2025-01-07 11:44:24 +0530

    after adding the booking section

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
       <div className="d-flex justify-content-center align-items-center w-100  aboutSectionMain py-5 py-md-0">
         <div className="row w-100 m-0 p-0  container">
           <div className="col-12 col-md-4 p-md-4">
-      <h1 className="d-block d-md-none text-center mb-5">About US</h1>
+            <h1 className="d-block d-md-none text-center mb-5">About US</h1>
             <img src="/profileCardImage.png" className="cardImage img-fluid" />
           </div>
           <div className="col-12 col-md-8 p-4">
@@ -34,49 +34,45 @@ export default function Home() {
       {/* about section end */}
 
       {/* booking section start */}
-      {/* <div className="d-flex justify-content-center align-items-center w-100  bookingSectionMain">
+      <div className="d-flex justify-content-center align-items-center w-100 bookingSectionMain">
         <div className="row w-100  container">
-          <div className="col-12 col-md-7 px-md-4 px-2 py-5">
-            <h1 className="mb-5">Booking Your Appointment</h1>
-            <div className="me-md-5 pe-md-5">
+          <div className="col-12 col-md-8 px-md-4 px-2 py-5">
+            <h1 className="mb-2">Booking Your Appointment</h1>
+            <div className="col-md-7 ">
               <label>Select Location</label>
               <slect className="form-control">
                 <option>Chandigarh</option>
               </slect>
-            
+
               <label>Full Name</label>
-              <input className="form-control"/>
+              <input className="form-control" />
               <label>Contact Number</label>
-              <input className="form-control"/>
+              <input className="form-control" />
               <label>Your City</label>
-              <input className="form-control"/>
+              <input className="form-control" />
               <label>Number of Kundali</label>
               <slect className="form-control">
                 <option>1 (10 min)</option>
               </slect>
               <label>Extra Time</label>
-              <input className="form-control"/>
+              <input className="form-control" />
               <label>Select Date</label>
-              <div>
-              <button>
-                20 Nov
-              </button>
-              <button>
-                21 Nov
-              </button>
-              <button>
-                22 Nov
-              </button>
+              <div className="mb-4 d-flex justify-content-between d-md-block">
+                <button className="purpleBtn">20 Nov</button>
+                <button className="whiteBtn mx-md-3">21 Nov</button>
+                <button className="whiteBtn">22 Nov</button>
               </div>
-              
+
               <div>
-                <button>Submit</button>
+                <button className=" submitBtn" style={{ background: "#139F01" }}>
+                  Submit
+                </button>
               </div>
             </div>
+            
           </div>
-          
         </div>
-      </div> */}
+      </div>
       {/* booking section end */}
     </div>
   );
